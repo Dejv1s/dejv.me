fa1ee804dc7c921dfad94bd7d80129f80741aa39
David Stuchlík <dejv1s.secret@gmail.com>
2024-10-31 21:52:28 +0100

    Deleted old web & created WIP page

--- 03/contact.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('email-icon').addEventListener('click', function() {
-        window.location.href = 'mailto:david@stuchlik.cz';
-    });
-});
